a1cd10465da2eac3c96be41227420942ab77920c
Iwan Kelaiah <iwan.kelaiah@gmail.com>
2024-11-18 15:45:09 +1100

    New - Initial commit

---
+++ src/threadsafelist.pas
@@ -0,0 +1,276 @@
+unit ThreadSafeList;
+
+{$mode objfpc}{$H+}
+{$modeswitch advancedrecords}
+
+interface
+
+uses
+  Classes, SysUtils, SyncObjs;
+
+type
+  // Generic comparer type
+  generic TComparer<T> = function(const A, B: T): Integer;
+
+  // Generic ThreadSafe List
+  generic TThreadSafeList<T> = class
+  private
+    FList: array of T;
+    FCount: Integer;
+    FCapacity: Integer;
+    FLock: TCriticalSection;
+    FComparer: specialize TComparer<T>;
+    FSorted: Boolean;
+    
+    procedure Grow;
+    procedure QuickSort(Left, Right: Integer);
+    function GetItem(Index: Integer): T;
+    procedure SetItem(Index: Integer; const Value: T);
+  public
+    constructor Create(AComparer: specialize TComparer<T>);
+    destructor Destroy; override;
+    
+    function Add(const Item: T): Integer;
+    procedure Delete(Index: Integer);
+    function Find(const Item: T): Integer;
+    function First: T;
+    function Last: T;
+    procedure Sort;
+    function IsSorted: Boolean;
+    procedure Replace(Index: Integer; const Item: T);
+    
+    property Items[Index: Integer]: T read GetItem write SetItem; default;
+    property Count: Integer read FCount;
+  end;
+
+// Basic comparers declarations
+function IntegerComparer(const A, B: Integer): Integer;
+function StringComparer(const A, B: string): Integer;
+function BooleanComparer(const A, B: Boolean): Integer;
+function RealComparer(const A, B: Real): Integer;
+
+implementation
+
+// Basic comparers implementation
+function IntegerComparer(const A, B: Integer): Integer;
+begin
+  Result := A - B;
+end;
+
+function StringComparer(const A, B: string): Integer;
+begin
+  Result := CompareStr(A, B);
+end;
+
+function BooleanComparer(const A, B: Boolean): Integer;
+begin
+  Result := Ord(A) - Ord(B);
+end;
+
+function RealComparer(const A, B: Real): Integer;
+begin
+  if A < B then
+    Result := -1
+  else if A > B then
+    Result := 1
+  else
+    Result := 0;
+end;
+
+{ TThreadSafeList }
+
+constructor TThreadSafeList.Create(AComparer: specialize TComparer<T>);
+begin
+  inherited Create;
+  if not Assigned(AComparer) then
+    raise Exception.Create('Comparer must be provided');
+    
+  FLock := TCriticalSection.Create;
+  FComparer := AComparer;
+  FCount := 0;
+  FCapacity := 0;
+  FSorted := True;
+end;
+
+destructor TThreadSafeList.Destroy;
+begin
+  FLock.Free;
+  inherited;
+end;
+
+procedure TThreadSafeList.Grow;
+begin
+  if FCapacity = 0 then
+    FCapacity := 4
+  else
+    FCapacity := FCapacity * 2;
+  SetLength(FList, FCapacity);
+end;
+
+procedure TThreadSafeList.QuickSort(Left, Right: Integer);
+var
+  I, J: Integer;
+  P, Temp: T;
+begin
+  if Right <= Left then Exit;
+  
+  I := Left;
+  J := Right;
+  P := FList[(Left + Right) div 2];
+  
+  repeat
+    while FComparer(FList[I], P) < 0 do Inc(I);
+    while FComparer(FList[J], P) > 0 do Dec(J);
+    
+    if I <= J then
+    begin
+      Temp := FList[I];
+      FList[I] := FList[J];
+      FList[J] := Temp;
+      Inc(I);
+      Dec(J);
+    end;
+  until I > J;
+  
+  if Left < J then QuickSort(Left, J);
+  if I < Right then QuickSort(I, Right);
+end;
+
+function TThreadSafeList.Add(const Item: T): Integer;
+begin
+  FLock.Acquire;
+  try
+    if FCount = FCapacity then
+      Grow;
+      
+    FList[FCount] := Item;
+    Result := FCount;
+    Inc(FCount);
+    
+    if FCount > 1 then
+      FSorted := FSorted and (FComparer(FList[FCount-2], Item) <= 0);
+  finally
+    FLock.Release;
+  end;
+end;
+
+procedure TThreadSafeList.Delete(Index: Integer);
+var
+  I: Integer;
+begin
+  FLock.Acquire;
+  try
+    if (Index < 0) or (Index >= FCount) then
+      raise Exception.Create('Index out of bounds');
+      
+    for I := Index to FCount - 2 do
+      FList[I] := FList[I + 1];
+    Dec(FCount);
+  finally
+    FLock.Release;
+  end;
+end;
+
+function TThreadSafeList.Find(const Item: T): Integer;
+var
+  I: Integer;
+begin
+  Result := -1;
+  FLock.Acquire;
+  try
+    for I := 0 to FCount - 1 do
+      if FComparer(FList[I], Item) = 0 then
+      begin
+        Result := I;
+        Break;
+      end;
+  finally
+    FLock.Release;
+  end;
+end;
+
+function TThreadSafeList.First: T;
+begin
+  FLock.Acquire;
+  try
+    if FCount = 0 then
+      raise Exception.Create('List is empty');
+    Result := FList[0];
+  finally
+    FLock.Release;
+  end;
+end;
+
+function TThreadSafeList.Last: T;
+begin
+  FLock.Acquire;
+  try
+    if FCount = 0 then
+      raise Exception.Create('List is empty');
+    Result := FList[FCount - 1];
+  finally
+    FLock.Release;
+  end;
+end;
+
+procedure TThreadSafeList.Sort;
+begin
+  FLock.Acquire;
+  try
+    if FCount > 1 then
+      QuickSort(0, FCount - 1);
+    FSorted := True;
+  finally
+    FLock.Release;
+  end;
+end;
+
+function TThreadSafeList.IsSorted: Boolean;
+begin
+  FLock.Acquire;
+  try
+    Result := FSorted;
+  finally
+    FLock.Release;
+  end;
+end;
+
+procedure TThreadSafeList.Replace(Index: Integer; const Item: T);
+begin
+  FLock.Acquire;
+  try
+    if (Index < 0) or (Index >= FCount) then
+      raise Exception.Create('Index out of bounds');
+    FList[Index] := Item;
+    
+    if FSorted then
+    begin
+      if (Index > 0) and (FComparer(FList[Index-1], Item) > 0) then
+        FSorted := False
+      else if (Index < FCount-1) and (FComparer(Item, FList[Index+1]) > 0) then
+        FSorted := False;
+    end;
+  finally
+    FLock.Release;
+  end;
+end;
+
+function TThreadSafeList.GetItem(Index: Integer): T;
+begin
+  FLock.Acquire;
+  try
+    if (Index < 0) or (Index >= FCount) then
+      raise Exception.Create('Index out of bounds');
+    Result := FList[Index];
+  finally
+    FLock.Release;
+  end;
+end;
+
+procedure TThreadSafeList.SetItem(Index: Integer; const Value: T);
+begin
+  Replace(Index, Value);
+end;
+
+end.
+ 
---
+++ tests/threadsafelisttests.pas
@@ -0,0 +1,562 @@
+unit ThreadSafeListTests;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, fpcunit, testregistry, ThreadSafeList, DateUtils;
+
+type
+  // Test thread for stress testing
+  TTestThread = class(TThread)
+  private
+    FList: specialize TThreadSafeList<Integer>;
+    FStartValue: Integer;
+    FOperations: Integer;
+  public
+    constructor Create(AList: specialize TThreadSafeList<Integer>;
+      AStartValue, AOperations: Integer);
+    procedure Execute; override;
+  end;
+
+  TThreadSafeListTest = class(TTestCase)
+  private
+    FIntList: specialize TThreadSafeList<Integer>;
+    FStrList: specialize TThreadSafeList<string>;
+    FBoolList: specialize TThreadSafeList<Boolean>;
+    FRealList: specialize TThreadSafeList<Real>;
+  protected
+    procedure SetUp; override;
+    procedure TearDown; override;
+  published
+    // Basic functionality tests
+    procedure TestCreation;
+    procedure TestCreationWithNilComparer;
+    procedure TestAddInteger;
+    procedure TestAddString;
+    procedure TestDelete;
+    procedure TestFind;
+    procedure TestFirstLast;
+    procedure TestReplace;
+    procedure TestSort;
+    procedure TestIsSorted;
+
+    // Edge cases
+    procedure TestEmptyList;
+    procedure TestSingleElement;
+    procedure TestDuplicateElements;
+    procedure TestBoundaries;
+
+    // Specific type tests
+    procedure TestBooleanList;
+    procedure TestRealList;
+
+    // Stress tests
+    procedure TestMultiThreadAccess;
+    procedure TestLargeDataSet;
+    procedure TestRandomOperations;
+    procedure TestLargeDataSetSortingPerformance;
+  end;
+
+implementation
+
+const
+  STRESS_THREAD_COUNT = 10;
+  OPERATIONS_PER_THREAD = 1000;
+  LARGE_DATASET_SIZE = 100000;
+
+{ TTestThread }
+
+constructor TTestThread.Create(AList: specialize TThreadSafeList<Integer>;
+  AStartValue, AOperations: Integer);
+begin
+  inherited Create(True);
+  FList := AList;
+  FStartValue := AStartValue;
+  FOperations := AOperations;
+  FreeOnTerminate := False;
+end;
+
+procedure TTestThread.Execute;
+var
+  I: Integer;
+  Operation: Integer;
+begin
+  for I := 0 to FOperations - 1 do
+  begin
+    Operation := Random(3); // 0: Add, 1: Delete, 2: Find
+    case Operation of
+      0: FList.Add(FStartValue + I);
+      1: if FList.Count > 0 then
+         try
+           FList.Delete(Random(FList.Count));
+         except
+           // Ignore concurrent deletion errors
+         end;
+      2: FList.Find(FStartValue + Random(I + 1));
+    end;
+  end;
+end;
+
+{ TThreadSafeListTest }
+
+procedure TThreadSafeListTest.SetUp;
+begin
+  FIntList := specialize TThreadSafeList<Integer>.Create(@IntegerComparer);
+  FStrList := specialize TThreadSafeList<string>.Create(@StringComparer);
+  FBoolList := specialize TThreadSafeList<Boolean>.Create(@BooleanComparer);
+  FRealList := specialize TThreadSafeList<Real>.Create(@RealComparer);
+end;
+
+procedure TThreadSafeListTest.TearDown;
+begin
+  FIntList.Free;
+  FStrList.Free;
+  FBoolList.Free;
+  FRealList.Free;
+end;
+
+procedure TThreadSafeListTest.TestCreation;
+begin
+  AssertNotNull('Integer list should be created', FIntList);
+  AssertEquals('New list should be empty', 0, FIntList.Count);
+  AssertTrue('New list should be sorted', FIntList.IsSorted);
+end;
+
+procedure TThreadSafeListTest.TestCreationWithNilComparer;
+var
+  TempList: specialize TThreadSafeList<Integer>;
+begin
+  try
+    TempList := specialize TThreadSafeList<Integer>.Create(nil);
+    Fail('Should raise exception with nil comparer');
+  except
+    on E: Exception do
+      AssertTrue('Correct exception raised', True);
+  end;
+end;
+
+procedure TThreadSafeListTest.TestAddInteger;
+begin
+  FIntList.Add(5);
+  FIntList.Add(3);
+  FIntList.Add(7);
+
+  AssertEquals('Count should be 3', 3, FIntList.Count);
+  AssertEquals('First element should be 5', 5, FIntList[0]);
+  AssertFalse('List should not be sorted', FIntList.IsSorted);
+end;
+
+procedure TThreadSafeListTest.TestAddString;
+begin
+  FStrList.Add('Hello');
+  FStrList.Add('World');
+
+  AssertEquals('Count should be 2', 2, FStrList.Count);
+  AssertEquals('First element should be Hello', 'Hello', FStrList[0]);
+end;
+
+procedure TThreadSafeListTest.TestDelete;
+begin
+  FIntList.Add(1);
+  FIntList.Add(2);
+  FIntList.Add(3);
+
+  FIntList.Delete(1);
+  AssertEquals('Count should be 2', 2, FIntList.Count);
+  AssertEquals('Second element should be 3', 3, FIntList[1]);
+
+  try
+    FIntList.Delete(5);
+    Fail('Should raise exception for invalid index');
+  except
+    on E: Exception do
+      AssertTrue('Correct exception raised', True);
+  end;
+end;
+
+procedure TThreadSafeListTest.TestMultiThreadAccess;
+var
+  Threads: array[0..STRESS_THREAD_COUNT-1] of TTestThread;
+  I: Integer;
+  StartTick, EndTick: QWord;
+begin
+  StartTick := GetTickCount64;
+
+  // Create and start threads
+  for I := 0 to STRESS_THREAD_COUNT-1 do
+  begin
+    Threads[I] := TTestThread.Create(FIntList, I * OPERATIONS_PER_THREAD,
+      OPERATIONS_PER_THREAD);
+    Threads[I].Start;
+  end;
+
+  // Wait for all threads to complete
+  for I := 0 to STRESS_THREAD_COUNT-1 do
+  begin
+    Threads[I].WaitFor;
+    Threads[I].Free;
+  end;
+
+  EndTick := GetTickCount64;
+  WriteLn(Format('Multi-thread operations took: %d ms', [EndTick - StartTick]));
+
+  // Verify list integrity
+  try
+    FIntList.Sort;
+    AssertTrue('List should be sortable after stress test', FIntList.IsSorted);
+  except
+    Fail('List integrity compromised during stress test');
+  end;
+end;
+
+procedure TThreadSafeListTest.TestLargeDataSet;
+var
+  I: Integer;
+  StartTick, EndTick: QWord;
+begin
+  StartTick := GetTickCount64;
+
+  // Add large number of items
+  for I := 0 to LARGE_DATASET_SIZE - 1 do
+    FIntList.Add(Random(LARGE_DATASET_SIZE));
+
+  // Sort and verify
+  FIntList.Sort;
+  AssertTrue('Large dataset should be sortable', FIntList.IsSorted);
+
+  // Verify order
+  for I := 1 to FIntList.Count - 1 do
+    AssertTrue('Items should be in order',
+      FIntList[I-1] <= FIntList[I]);
+
+  EndTick := GetTickCount64;
+  WriteLn(Format('Large dataset operations took: %d ms', [EndTick - StartTick]));
+
+  // Performance check (adjust threshold as needed)
+  AssertTrue('Large dataset operations should complete within reasonable time',
+    EndTick - StartTick < 5000);
+end;
+
+procedure TThreadSafeListTest.TestRandomOperations;
+var
+  I, Idx: Integer;
+  Operations: Integer;
+begin
+  Operations := 10000;
+
+  for I := 1 to Operations do
+  begin
+    case Random(4) of
+      0: FIntList.Add(Random(1000));
+      1: if FIntList.Count > 0 then
+         begin
+           Idx := Random(FIntList.Count);
+           FIntList.Replace(Idx, Random(1000));
+         end;
+      2: if FIntList.Count > 0 then
+         begin
+           Idx := Random(FIntList.Count);
+           FIntList.Delete(Idx);
+         end;
+      3: FIntList.Find(Random(1000));
+    end;
+  end;
+
+  // Verify list integrity
+  AssertTrue('List should survive random operations', True);
+end;
+
+procedure TThreadSafeListTest.TestFind;
+begin
+  FIntList.Add(1);
+  FIntList.Add(2);
+  FIntList.Add(3);
+
+  AssertEquals('Should find existing element', 1, FIntList.Find(2));
+  AssertEquals('Should return -1 for non-existing element', -1, FIntList.Find(5));
+end;
+
+procedure TThreadSafeListTest.TestFirstLast;
+begin
+  FIntList.Add(1);
+  FIntList.Add(2);
+  FIntList.Add(3);
+
+  AssertEquals('First should be 1', 1, FIntList.First);
+  AssertEquals('Last should be 3', 3, FIntList.Last);
+
+  try
+    FIntList := specialize TThreadSafeList<Integer>.Create(@IntegerComparer);
+    FIntList.First;
+    Fail('Should raise exception on empty list First');
+  except
+    on E: Exception do
+      AssertTrue('Correct exception raised', True);
+  end;
+
+  try
+    FIntList.Last;
+    Fail('Should raise exception on empty list Last');
+  except
+    on E: Exception do
+      AssertTrue('Correct exception raised', True);
+  end;
+end;
+
+procedure TThreadSafeListTest.TestReplace;
+begin
+  FIntList.Add(1);
+  FIntList.Add(2);
+  FIntList.Add(3);
+
+  FIntList.Replace(1, 5);
+  AssertEquals('Element should be replaced', 5, FIntList[1]);
+
+  try
+    FIntList.Replace(10, 5);
+    Fail('Should raise exception for invalid index');
+  except
+    on E: Exception do
+      AssertTrue('Correct exception raised', True);
+  end;
+end;
+
+procedure TThreadSafeListTest.TestSort;
+begin
+  FIntList.Add(3);
+  FIntList.Add(1);
+  FIntList.Add(2);
+
+  FIntList.Sort;
+  AssertEquals('First element after sort', 1, FIntList[0]);
+  AssertEquals('Second element after sort', 2, FIntList[1]);
+  AssertEquals('Third element after sort', 3, FIntList[2]);
+  AssertTrue('List should be sorted', FIntList.IsSorted);
+end;
+
+procedure TThreadSafeListTest.TestIsSorted;
+begin
+  AssertTrue('Empty list should be sorted', FIntList.IsSorted);
+
+  FIntList.Add(1);
+  AssertTrue('Single element list should be sorted', FIntList.IsSorted);
+
+  FIntList.Add(2);
+  AssertTrue('Ascending elements should be sorted', FIntList.IsSorted);
+
+  FIntList.Add(1);
+  AssertFalse('List should not be sorted after adding smaller element', FIntList.IsSorted);
+end;
+
+procedure TThreadSafeListTest.TestEmptyList;
+begin
+  AssertEquals('Empty list count should be 0', 0, FIntList.Count);
+  AssertTrue('Empty list should be sorted', FIntList.IsSorted);
+  AssertEquals('Find in empty list should return -1', -1, FIntList.Find(1));
+end;
+
+procedure TThreadSafeListTest.TestSingleElement;
+begin
+  FIntList.Add(1);
+  AssertEquals('Single element list count should be 1', 1, FIntList.Count);
+  AssertTrue('Single element list should be sorted', FIntList.IsSorted);
+  AssertEquals('First and last should be same', FIntList.First, FIntList.Last);
+end;
+
+procedure TThreadSafeListTest.TestDuplicateElements;
+begin
+  FIntList.Add(1);
+  FIntList.Add(1);
+  FIntList.Add(1);
+
+  AssertEquals('Count should be 3', 3, FIntList.Count);
+  AssertTrue('List with duplicates should be sorted', FIntList.IsSorted);
+  AssertEquals('Should find first occurrence', 0, FIntList.Find(1));
+end;
+
+procedure TThreadSafeListTest.TestBoundaries;
+begin
+  try
+    FIntList.Delete(-1);
+    Fail('Should raise exception for negative index');
+  except
+    on E: Exception do
+      AssertTrue('Correct exception raised', True);
+  end;
+
+  try
+    FIntList.GetItem(0);
+    Fail('Should raise exception for empty list access');
+  except
+    on E: Exception do
+      AssertTrue('Correct exception raised', True);
+  end;
+end;
+
+procedure TThreadSafeListTest.TestBooleanList;
+begin
+  FBoolList.Add(True);
+  FBoolList.Add(False);
+  FBoolList.Add(True);
+
+  FBoolList.Sort;
+  AssertEquals('First element should be False', False, FBoolList[0]);
+  AssertEquals('Last element should be True', True, FBoolList[2]);
+end;
+
+procedure TThreadSafeListTest.TestRealList;
+begin
+  FRealList.Add(3.14);
+  FRealList.Add(1.41);
+  FRealList.Add(2.71);
+
+  FRealList.Sort;
+  AssertTrue('First element should be smallest', FRealList[0] < FRealList[1]);
+  AssertTrue('Last element should be largest', FRealList[1] < FRealList[2]);
+end;
+
+procedure TThreadSafeListTest.TestLargeDataSetSortingPerformance;
+const
+  SORT_SIZE = 100000;
+var
+  I: Integer;
+  StartTick, EndTick: QWord;
+  TempStr: string;
+  CharSet: string;
+  IsSortedCorrectly: Boolean;
+begin
+  CharSet := 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  // Test Integer sorting
+  WriteLn('Integer Test:');
+  for I := 0 to SORT_SIZE - 1 do
+    FIntList.Add(SORT_SIZE - I);  // Add in reverse order to ensure we need sorting
+    
+  // Print first few elements before sort
+  WriteLn('Before sort (first 5): ');
+  for I := 0 to 4 do
+    Write(FIntList[I], ' ');
+  WriteLn;
+    
+  StartTick := GetTickCount64;
+  FIntList.Sort;
+  EndTick := GetTickCount64;
+  WriteLn(Format('Sorting %d integers took: %d ms', [SORT_SIZE, EndTick - StartTick]));
+  
+  // Print first few elements after sort
+  WriteLn('After sort (first 5): ');
+  for I := 0 to 4 do
+    Write(FIntList[I], ' ');
+  WriteLn;
+    
+  // Verify sorting thoroughly
+  IsSortedCorrectly := True;
+  for I := 1 to FIntList.Count - 1 do
+    if FIntList[I-1] > FIntList[I] then
+    begin
+      WriteLn(Format('Sort error at position %d: %d > %d', 
+        [I, FIntList[I-1], FIntList[I]]));
+      IsSortedCorrectly := False;
+      Break;
+    end;
+  
+  AssertTrue('Integer list should be properly sorted', IsSortedCorrectly);
+  WriteLn('Integer sort verification: ', IsSortedCorrectly);
+  WriteLn;
+
+  FIntList.Free;
+  FIntList := specialize TThreadSafeList<Integer>.Create(@IntegerComparer);
+
+  // Test String sorting
+  WriteLn('String Test:');
+  for I := 0 to SORT_SIZE - 1 do
+  begin
+    SetLength(TempStr, 10);
+    TempStr := '';
+    while Length(TempStr) < 10 do
+      TempStr := TempStr + CharSet[Random(Length(CharSet)) + 1];
+    FStrList.Add(TempStr);
+  end;
+
+  // Print first few strings before sort
+  WriteLn('Before sort (first 3): ');
+  for I := 0 to 2 do
+    WriteLn(FStrList[I]);
+    
+  StartTick := GetTickCount64;
+  FStrList.Sort;
+  EndTick := GetTickCount64;
+  WriteLn(Format('Sorting %d strings (10 chars each) took: %d ms', 
+    [SORT_SIZE, EndTick - StartTick]));
+
+  // Print first few strings after sort
+  WriteLn('After sort (first 3): ');
+  for I := 0 to 2 do
+    WriteLn(FStrList[I]);
+    
+  // Verify string sorting
+  IsSortedCorrectly := True;
+  for I := 1 to FStrList.Count - 1 do
+    if CompareStr(FStrList[I-1], FStrList[I]) > 0 then
+    begin
+      WriteLn(Format('Sort error at position %d: %s > %s', 
+        [I, FStrList[I-1], FStrList[I]]));
+      IsSortedCorrectly := False;
+      Break;
+    end;
+    
+  AssertTrue('String list should be properly sorted', IsSortedCorrectly);
+  WriteLn('String sort verification: ', IsSortedCorrectly);
+  WriteLn;
+
+  FStrList.Free;
+  FStrList := specialize TThreadSafeList<string>.Create(@StringComparer);
+
+  // Test Real sorting
+  WriteLn('Real Test:');
+  for I := 0 to SORT_SIZE - 1 do
+    FRealList.Add((SORT_SIZE - I) * 1.5);  // Add in reverse order with decimals
+    
+  // Print first few elements before sort
+  WriteLn('Before sort (first 5): ');
+  for I := 0 to 4 do
+    Write(Format('%.2f ', [FRealList[I]]));
+  WriteLn;
+    
+  StartTick := GetTickCount64;
+  FRealList.Sort;
+  EndTick := GetTickCount64;
+  WriteLn(Format('Sorting %d reals took: %d ms', [SORT_SIZE, EndTick - StartTick]));
+  
+  // Print first few elements after sort
+  WriteLn('After sort (first 5): ');
+  for I := 0 to 4 do
+    Write(Format('%.2f ', [FRealList[I]]));
+  WriteLn;
+    
+  // Verify real sorting
+  IsSortedCorrectly := True;
+  for I := 1 to FRealList.Count - 1 do
+    if FRealList[I-1] > FRealList[I] then
+    begin
+      WriteLn(Format('Sort error at position %d: %.2f > %.2f', 
+        [I, FRealList[I-1], FRealList[I]]));
+      IsSortedCorrectly := False;
+      Break;
+    end;
+    
+  AssertTrue('Real list should be properly sorted', IsSortedCorrectly);
+  WriteLn('Real sort verification: ', IsSortedCorrectly);
+  WriteLn;
+
+  FRealList.Free;
+  FRealList := specialize TThreadSafeList<Real>.Create(@RealComparer);
+end;
+
+initialization
+
+  RegisterTest(TThreadSafeListTest);
+end.
+
